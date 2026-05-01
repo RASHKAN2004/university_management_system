8ae6a92c3c7fc85ffe1e16a4effebe1f4dfcc90f
Aysha-28 <aysha12ays@gmail.com>
2026-05-01 23:44:53 +0530

    added sample data to tables

---
+++ sample_data.sql
@@ -0,0 +1,30 @@
+insert into students values
+    ('ICT001', 'Sara', '200417283718','sara@gmail.com', '0771299782','colombo','proper'),
+    ('ICT002', 'John', '200689251673','john@gmail.com', '0751368978','kandy','repeat'),
+    ('ICT003', 'Nimal','200496571315','nimal@gmail.com','0772126196','galle','proper'),
+    ('ICT004', 'Sunil','200412334754','sunil@gmail.com','0752364293','kegalle','repeat'),
+    ('ICT005', 'Kamal','200412223437','kamal@gmail.com','0752364297','Mannar','proper'),
+    ('ICT006', 'Nivetha','200431323733','nivetha@gmail.com','0761363946','kandy','proper'),
+    ('ICT007', 'Amna','200415222318','amna@gmail.com','0773213933','matara','proper'),
+    ('ICT008', 'Durga','200418327416','durga@gmail.com','0773363973','jaffna','repeat'),
+    ('ICT009', 'Thilini','200414222714','thilini@gmail.com','0743364171','Badulla','proper'),
+    ('ICT0010', 'Amila', '200413332361','amila@gmail.com','0752363975','Kalutara','proper'),
+    ('ICT0011', 'silva', '200467893710','silva@gmail.com', '0771556293','Gampaha','proper'),
+    ('ICT0012', 'Dilani','200413563311','dilani@gmail.com','0754364974','jaffna','repeat'),
+    ('ICT0013', 'Supun', '200463884312','supun@gmail.com', '0741358723','kegalle','repeat'),
+    ('ICT0014', 'Ishara','200417123732','ishara@gmail.com','0754363911','Ampara','proper'),
+    ('ICT0015', 'Perera','200413237173','perera@gmail.com','0771314779','Ratnapura','proper');
+
+insert into lecturers values
+   (1,'Dr.Nimal','nimal@gmail.com', '0778925711','ICT','PhD in Computer Science'),
+   (2,'Dr.Chamara','chamara@gmail.com', '0772225212','ICT','MSc in Software Engineering'),
+   (3,'Dr.Malini','malini@gmail.com', '0771225314','ICT','BSc in Information Technology'),
+   (4,'Dr.Ravindu','ravindu@gmail.com', '0741322271','ICT','MSc in Data Science'),
+   (5,'Dr.Ruwan','ruwan@gmail.com', '0771225313','ICT','PhD in Artificial Intelligence');
+
+insert into technical_officers values
+   (1,'saman','saman@gmail.com','0751238952','Networking Lab','Morning','5 years'),
+   (2,'pradeep','pradeep@gmail.com','0771232394','Hardware Lab','Morning','4 years'),
+   (3,'Jayasinge','jayasinge@gmail.com','0771237458','Multimedia Lab','Evening','3 years'),
+   (4,'Fernando','fernando@gmail.com','0752333393','Software Lab','Morning','4 years'),
+   (5,'Kumara','kumara@gmail.com','0771332951','Database Lab','Evening','2 years');
