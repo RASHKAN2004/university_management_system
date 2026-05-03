5725e0ae68e0eefe6202f5373a2131511bee9eee
RASHKAN2004 <mmohamedraskhan@gmail.com>
2026-05-03 18:49:27 +0530

    debeg tables data

--- db_tables.sql
+++ db_tables.sql
@@ -1,74 +1,72 @@
-create database FacultyManagement;
-use FacultyManagement;
+CREATE DATABASE FacultyManagement;
+USE FacultyManagement;
 
-create table students(
- reg_no varchar(10) primary key, 
- name varchar(50),
- nic varchar(50),
- email varchar(50),
- phone varchar(15),
- address varchar(50),
- status enum('proper', 'repeat', 'suspended')
- );
-
- create table lecturers(
-  lec_id int primary key auto_increment,
-  name varchar(50),
-  email varchar(50),
-  phone varchar(15),
-  department varchar(50),
-  qualification varchar(50)
- );
-
-create table technical_officers(
-  tech_officer_id int primary key auto_increment,
-  name varchar(50),
-  email varchar(50),
-  phone varchar(15),
-  lab varchar(50),
-  shift_time varchar(50),
-  experience varchar(15)
+CREATE TABLE students(
+    reg_no VARCHAR(15) PRIMARY KEY, 
+    name VARCHAR(50),
+    nic VARCHAR(50),
+    email VARCHAR(50),
+    phone VARCHAR(15),
+    address VARCHAR(100),
+    status ENUM('proper', 'repeat', 'suspended')
 );
 
-create table courses(
-  course_code varchar(15) primary key,
-  name varchar(50),
-  credits int,
-  type enum('theory', 'practical'),
-  lec_id int,
-  foreign key(lec_id) references lecturers(lec_id)
+CREATE TABLE lecturers(
+    lec_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    email VARCHAR(50),
+    phone VARCHAR(15),
+    department VARCHAR(50),
+    qualification VARCHAR(100)
 );
 
-create table attendance(
-  attendance_id int primary key auto_increment,
-  reg_no varchar(10),
-  course_code varchar(15),
-  type enum('THEORY','PRACTICAL'),
-  week int,
-  status enum('PRESENT','ABSENT','MEDICAL'),
-  foreign key(reg_no) references students(reg_no),
-  foreign key(course_code) references courses(course_code)
+CREATE TABLE technical_officers(
+    tech_officer_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    email VARCHAR(50),
+    phone VARCHAR(15),
+    lab VARCHAR(50),
+    shift_time VARCHAR(50),
+    experience VARCHAR(20)
 );
 
-create table marks(
-  mark_id int primary key auto_increment,
-  reg_no varchar(10),
-  course_code varchar(15),
-  type ENUM('QUIZ','ASSESSMENT','MID_THEORY','MID_PRACTICAL','FINAL_THEORY','FINAL_PRACTICAL'),
-  marks int,
-  foreign key(reg_no) references students(reg_no),
-  foreign key(course_code) references courses(course_code)    
+CREATE TABLE courses(
+    course_code VARCHAR(15) PRIMARY KEY,
+    name VARCHAR(50),
+    credits INT,
+    type ENUM('theory', 'practical'),
+    lec_id INT,
+    FOREIGN KEY(lec_id) REFERENCES lecturers(lec_id)
 );
 
+CREATE TABLE attendance(
+    attendance_id INT PRIMARY KEY AUTO_INCREMENT,
+    reg_no VARCHAR(15),
+    course_code VARCHAR(15),
+    type ENUM('THEORY','PRACTICAL'),
+    week INT,
+    status ENUM('PRESENT','ABSENT','MEDICAL'),
+    FOREIGN KEY(reg_no) REFERENCES students(reg_no),
+    FOREIGN KEY(course_code) REFERENCES courses(course_code)
+);
+
+CREATE TABLE marks(
+    mark_id INT PRIMARY KEY AUTO_INCREMENT,
+    reg_no VARCHAR(15),
+    course_code VARCHAR(15),
+    type ENUM('QUIZ','ASSESSMENT','MID_THEORY','MID_PRACTICAL','FINAL_THEORY','FINAL_PRACTICAL'),
+    marks INT,
+    FOREIGN KEY(reg_no) REFERENCES students(reg_no),
+    FOREIGN KEY(course_code) REFERENCES courses(course_code)    
+);
 
 CREATE TABLE results (
-    result_id int primary key auto_increment,
-    reg_no varchar(20),
-    course_code varchar(10),
-    final_marks decimal(5,2),
-    grade varchar(5),
-    gpa decimal(3,2),
-    foreign key(reg_no) references students(reg_no),
-  foreign key(course_code) references courses(course_code)  
+    result_id INT PRIMARY KEY AUTO_INCREMENT,
+    reg_no VARCHAR(15),
+    course_code VARCHAR(15),
+    final_marks DECIMAL(5,2),
+    grade VARCHAR(5),
+    gpa DECIMAL(3,2),
+    FOREIGN KEY(reg_no) REFERENCES students(reg_no),
+    FOREIGN KEY(course_code) REFERENCES courses(course_code)  
 );
-
